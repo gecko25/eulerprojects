91e2e98a6366fdc4111bed981c2d6a7adf37572f
gecko25 <sara.tankard@gmail.com>
2015-07-18 18:14:08 -0400

    added solution to problem 2

--- solutions/js/app.js
+++ solutions/js/app.js
@@ -4,18 +4,17 @@
 
 	/******* PROBLEM ONE *********/
 	app.controller("ProblemOneController", function(){
-		this.answer = 0;
 		var sum = 0; // keep track of "passed" variables
 
 		//loop through all numbers 1-1000
 		for (var i=1; i<1000; i++){
 
 			if (i%3===0){
-				sum=sum+i;
+				sum+=i;
 				continue;
 
 			}else if (i%5===0){
-				sum=sum+i;
+				sum+=i;
 				continue;
 
 			}else{
@@ -24,7 +23,37 @@
 		}
 
 		this.answer = sum;
-		console.log("The sum is " + sum);
 }); // closes app.controller 
+
+
+	/******* PROBLEM ONE *********/
+	app.controller("ProblemTwoController", function(){
+		var max = 4000000;  //limit for calcuating sum
+		var previous_value = 1;  //1st in fib sequence  (1,2,3,5,8..)
+		var current_value = 2;   //2nd value in fib
+		var next_value; //3
+
+		sumOfEvenValues = 2; // initialize to 2 so we can test current_value for evenness
+
+		do{
+			//calculate fib seq
+			next_value = previous_value + current_value;
+
+			//determine if even
+			if (next_value%2==0){
+				sumOfEvenValues+=next_value;
+			}
+
+			//shift the sequence
+			previous_value = current_value;
+			current_value = next_value;
+
+
+		}while (current_value<max);
+
+		this.answer = sumOfEvenValues;  //controllerobject.answer
+
+
+}); // closes app.controller 	
 
 })();  //closes closure function
